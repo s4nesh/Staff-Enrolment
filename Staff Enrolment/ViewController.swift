27bfb87812d4257d6f84f06b77fc674beba98852
Sanesh Gobin <saneshgobin@icloud.com>
2023-07-12 18:53:39 +0200

    Add logic for staff number generator and display label

--- Staff Enrolment/ViewController.swift
+++ Staff Enrolment/ViewController.swift
@@ -10,17 +10,45 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var popUpView: UIView!
-    
+    @IBOutlet weak var nameInput: UITextField!
+    @IBOutlet weak var ageInput: UITextField!
+    @IBOutlet weak var jobTitleInput: UITextField!
     @IBOutlet weak var enrollButtonSelect: UIButton!
+
+    @IBOutlet weak var displayLabel: UILabel!
+    @IBOutlet weak var staffNumber: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         popUpView.isHidden = true
-        enrollButtonSelect.isEnabled = false
+//        enrollButtonSelect.isEnabled = false
     }
 
     @IBAction func addEmpButton(_ sender: UIButton) {
         popUpView.isHidden = false
+    }
+    
+
+    
+    
+    // Popup screen buttons
+    var staffNumberArray = [Int]()
+    func getStaffNumber() -> Int{
+        let countOfStaffNumbers = staffNumberArray.count
+        return countOfStaffNumbers + 1
+    }
+    
+    @IBAction func numberGeneratorButtonPressed(_ sender: UIButton) {
+        let newStaffNumber = getStaffNumber()
+        staffNumber!.text = "Staff number: \(String(newStaffNumber))"
+        staffNumberArray.append(newStaffNumber)
+        
+    }
+    
+    @IBAction func enrolledButtonPressed(_ sender: UIButton) {
+        
+        
     }
     
 }
